24ae511b31eae2123e40bd0ecdadf02c036a0c73
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-12-07 09:20:29 +0100

    day 7 complete :)

---
+++ day7/problem1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 09:08:28 2021
+
+@author: Anja
+"""
+
+import numpy as np
+
+
+def minCosts(filename):
+    data = np.loadtxt(filename, delimiter=',')
+    
+    minH = int(data.min())
+    maxH = int(data.max())
+    
+    posH = [i for i in range(minH, maxH+1)]
+    
+    costs = []
+    
+    for pos in posH:
+        fuel = 0
+        for d in data:
+            fuel += abs(pos - d)
+        costs.append(fuel)
+            
+    return min(costs)
+
+print(minCosts('sample.txt'))
+print(minCosts('input.txt'))
